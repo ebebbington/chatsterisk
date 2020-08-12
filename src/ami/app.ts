3f775bd4ff0b5b48c31f5d2f30a078f5bac35e25
Edward Bebbington <edward.bebbington@intercity.technology>
2020-08-12 17:28:07 +0100

    moved ami socket into its own containner again

--- src/ami/app.ts
+++ src/ami/app.ts
@@ -12,44 +12,45 @@ console.log(
     `Socket server started on ws://${socketServer.hostname}:${socketServer.port}`,
 );
 
-const AMI = new SocketServer();
+const amiSocket = await Deno.connect({ hostname: "0.0.0.0", port: 5038 });
+console.log(amiSocket)
 
-async function getExtensionsFromAsterisk (): Promise<string[]> {
-  //const cmd = `/usr/sbin/asterisk -rx 'sip show peers' | awk -F'/' '{print $1}' | awk 'NR>2 {print last} {last=$0}'`
-  const cmd = ["asterisk", "-rx", `sip show peers`]
-  const p = await Deno.run({
-    cmd: cmd,
-    stdout: "piped",
-    stdin: "piped",
-  })
-  const stdout = new TextDecoder().decode(await p.output())
-  let lines: string[] = stdout.split("\n").filter(line => !isNaN(Number(line[0]))) // remove lines without numbers at start
-  lines = lines.filter(line => line.split(" ")[0].indexOf("/") > -1) // remove lines that are extensions eg title
-  let extensions: string[] = []
-  lines.forEach(line => {
-    const tmpLine = line.split(" ") // eg line = "6006/6006      192.15.6.4.2    D N"
-    if (tmpLine[0].match(/\d\/\d/)) { // eg "<number>/<number>"
-      const extension = tmpLine[0].split("/")[0]
-      extensions.push(extension)
-    }
-  });
-  const status = await p.status()
-  await p.close()
-  return extensions
-}
-
-async function makeCallToAsterisk (extensionToCall: number, extensionCalledFrom: number): Promise<void> {
-  const cmd = ["asterisk", "-rx", `originate local/${extensionToCall}@from-internal extension ${extensionCalledFrom}@from-internal`]
-  console.log(cmd)
-  const p = await Deno.run({
-    cmd: cmd,
-    stdout: "piped",
-    stdin: "piped"
-  })
-  console.log(await p.status())
-  const stdout = new TextDecoder().decode(await p.output())
-  p.close()
-}
+// async function getExtensionsFromAsterisk (): Promise<string[]> {
+//   //const cmd = `/usr/sbin/asterisk -rx 'sip show peers' | awk -F'/' '{print $1}' | awk 'NR>2 {print last} {last=$0}'`
+//   const cmd = ["asterisk", "-rx", `sip show peers`]
+//   const p = await Deno.run({
+//     cmd: cmd,
+//     stdout: "piped",
+//     stdin: "piped",
+//   })
+//   const stdout = new TextDecoder().decode(await p.output())
+//   let lines: string[] = stdout.split("\n").filter(line => !isNaN(Number(line[0]))) // remove lines without numbers at start
+//   lines = lines.filter(line => line.split(" ")[0].indexOf("/") > -1) // remove lines that are extensions eg title
+//   let extensions: string[] = []
+//   lines.forEach(line => {
+//     const tmpLine = line.split(" ") // eg line = "6006/6006      192.15.6.4.2    D N"
+//     if (tmpLine[0].match(/\d\/\d/)) { // eg "<number>/<number>"
+//       const extension = tmpLine[0].split("/")[0]
+//       extensions.push(extension)
+//     }
+//   });
+//   const status = await p.status()
+//   await p.close()
+//   return extensions
+// }
+//
+// async function makeCallToAsterisk (extensionToCall: number, extensionCalledFrom: number): Promise<void> {
+//   const cmd = ["asterisk", "-rx", `originate local/${extensionToCall}@from-internal extension ${extensionCalledFrom}@from-internal`]
+//   console.log(cmd)
+//   const p = await Deno.run({
+//     cmd: cmd,
+//     stdout: "piped",
+//     stdin: "piped"
+//   })
+//   console.log(await p.status())
+//   const stdout = new TextDecoder().decode(await p.output())
+//   p.close()
+// }
 
 socketServer.createChannel("test").onMessage((data: any) => {
   console.log('test got message')
@@ -59,14 +60,14 @@ socketServer.createChannel("test").onMessage((data: any) => {
 socketServer.createChannel("make-call").onMessage(async (data: any) => {
   console.log('data was recieved for make call')
   console.log(data)
-  await makeCallToAsterisk(data.message.to_extension, data.message.from_extension)
+  //await makeCallToAsterisk(data.message.to_extension, data.message.from_extension)
   //socketServer.to("made-call", JSON.stringify({ success: true, message: "done", data: null}))
 });
 
 socketServer.createChannel("get-extensions").onMessage(async (data: any) => {
   console.log("get-extensions called")
-  const extensions = await getExtensionsFromAsterisk()
-  socketServer.to("get-extensions", JSON.stringify({ success: true, message: "done", data: extensions }))
+  //const extensions = await getExtensionsFromAsterisk()
+  //socketServer.to("get-extensions", JSON.stringify({ success: true, message: "done", data: extensions }))
 })
 
 socketServer.on("connection", () => {
