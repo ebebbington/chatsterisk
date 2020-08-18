cdcd9d072b8783f35cce8aa18df762ae04bef062
Edward Bebbington <edward.bebbington@intercity.technology>
2020-08-18 11:19:54 +0100

    Add basic test placeholder for ami

---
+++ src/ami/tests/app_test.ts
@@ -0,0 +1,12 @@
+import {Rhum} from "../../server/tests/deps.ts";
+
+Rhum.testPlan("tests/app_test.ts", () => {
+  Rhum.testSuite("Socket", () => {
+    Rhum.testCase("Returns the extension list when asked", () => {
+      // Do socket.on(get-extensions)
+      // do socket.to(get-extensions)
+    })
+  })
+})
+
+Rhum.run()
