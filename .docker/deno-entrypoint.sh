55f00af9f2fcf23502ed22b77a345051d3e36b12
Edward Bebbington <edward.bebbington@intercity.technology>
2020-10-04 16:01:13 +0100

    update versions in docker env

--- .docker/deno-entrypoint.sh
+++ .docker/deno-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$USE_DENON" == "true" ]
 then
-  deno install --allow-read --allow-run --allow-write --allow-net -f -q --unstable https://deno.land/x/denon@2.3.2/denon.ts;
+  deno install --allow-read --allow-run --allow-write --allow-net -f -q --unstable https://deno.land/x/denon/denon.ts;
   export PATH="/root/.deno/bin:$PATH"
   denon start
 else
