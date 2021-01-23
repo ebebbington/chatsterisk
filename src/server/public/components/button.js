c1d7b45d929d8be565c3344e11b196be37d10dcb
Edward Bebbington <edward.bebbington@intercity.technology>
2021-01-23 00:11:51 +0000

    add new comp; expand upon example comp for docs

--- src/server/public/components/button.js
+++ src/server/public/components/button.js
@@ -1,8 +1,34 @@
-class Button extends HTMLElement {
-  connectedCallback() {
-    this.text = this.getAttribute("text")
-    this.innerHTML = `<button>${this.text}</button>`;
-  }
-}
+import { DestinyElement, html, reactive, register } from "https://code.okku.dev/destiny-ui/0.4.1/dist/mod.js";
 
-customElements.define('wc-button', Button);
+const styling = `
+<style>
+button {
+    color: var(--main-colour);
+    background-color: var(--sub-colour);
+    padding: .5em;
+    outline: 0;
+    border: 0;
+    border-radius: 5px;
+    font-size: 1em;
+    transition-duration: .3s;
+}
+button.success {
+    background: green;
+}
+button.error {
+    background: red;
+}
+button:hover {
+    cursor: pointer;
+    box-shadow: 0px 0px 2px 2px var(--ter-colour);
+}
+</style>
+`;
+
+register(class AButton extends HTMLElement {
+  connectedCallback() {
+    this.originalInnerText = this.innerText
+    this.innerHTML = `${styling}<button>${this.originalInnerText}</button>
+`;
+  }
+})
---
+++ src/server/public/components/input_row.js
@@ -0,0 +1,66 @@
+import { DestinyElement, html, reactive, register } from "https://code.okku.dev/destiny-ui/0.4.1/dist/mod.js";
+
+const styling = `
+<style>
+.input-row {
+    display: flex;
+    text-align: center;
+    margin: .5em;
+}
+.input-row input {
+    border: 1px solid var(--sub-colour);
+    text-align: center;
+    height: 28px;
+    border-radius: 5px 0 0 5px;
+    margin: 0 0 0 auto;
+    width: 100%;
+    padding: .5em;
+    font-size: 1em;
+}
+.input-row > button {
+    background-color: var(--main-colour);
+    border: 1px solid var(--sub-colour);
+    color: var(--sub-colour);
+    border-radius: 0 5px 5px 0;
+    padding: 0;
+    font-size: 1.2em;
+    width: 30px;
+    border-left: 0;
+    margin: 0 auto 0 0;
+}
+.input-row > button:hover {
+    cursor: pointer;
+    background-color: var(--sub-colour);
+    color: var(--main-colour);
+}
+.input-row > button.delete {
+    color: red;
+}
+.input-row > button.delete:hover {
+    color: white;
+}
+</style>
+`;
+
+/**
+ * EG
+ *
+ * <input-row container-id="new-todo-holder" input-id="new-todo-title" input-placeholder="Walk the dog" button-id="submit-new-todo"></input-row>
+ * <script src="/public/components/input_row.js" type="module"></script>
+ */
+register(class InputRow extends HTMLElement {
+  connectedCallback() {
+    this.containerId = this.getAttribute("container-id")
+    this.inputId = this.getAttribute("input-id")
+    this.inputPlaceHolder = this.getAttribute("input-placeholder")
+    this.buttonId = this.getAttribute("button-id")
+    this.innerHTML =
+      `
+${styling}
+<div class="input-row" id="${this.containerId}">
+    <input id="${this.inputId}" type="text" placeholder="${this.inputPlaceHolder}">
+    <button id="${this.buttonId}" type="button">&#10004</button>
+</div>
+`;
+  }
+})
