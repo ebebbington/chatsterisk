658720cb1a46b3d4f4f0fca25fb53b004d43c724
Edward Bebbington <edward.bebbington@intercity.technology>
2021-01-23 17:09:56 +0000

    cleanup

--- src/server/public/components/Call.ts
+++ src/server/public/components/Call.ts
@@ -1,9 +1,4 @@
-import {
-  DestinyElement,
-  html,
-  reactive,
-  register,
-} from "https://code.okku.dev/destiny-ui/0.4.1/dist/mod.js";
+import { register } from "https://code.okku.dev/destiny-ui/0.4.1/dist/mod.js";
 // TODO :: Make a dep file for destiny
 import { createWebSocketClient } from "../js/socket-client.ts";
 
--- src/server/public/components/Video.ts
+++ src/server/public/components/Video.ts
@@ -1,9 +1,4 @@
-import {
-  DestinyElement,
-  html,
-  reactive,
-  register,
-} from "https://code.okku.dev/destiny-ui/0.4.1/dist/mod.js";
+import { register } from "https://code.okku.dev/destiny-ui/0.4.1/dist/mod.js";
 import { createWebSocketClient } from "../js/socket-client.ts";
 
 // deno-lint-ignore no-undef
