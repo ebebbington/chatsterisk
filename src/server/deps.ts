1d46c02ce65633fbf3d1aefd63b4579a385e85dc
ebebbington <ebebbington@users.noreply.github.com>
2021-01-30 00:46:08 +0000

    Update dependencies

--- src/server/deps.ts
+++ src/server/deps.ts
@@ -1,4 +1,4 @@
-export { Drash } from "https://deno.land/x/drash@v1.4.0/mod.ts";
+export { Drash } from "https://deno.land/x/drash@v1.4.1/mod.ts";
 export { Paladin } from "https://deno.land/x/drash_middleware@v0.7.2/paladin/mod.ts";
 export { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
 export { Tengine } from "https://deno.land/x/drash_middleware@v0.7.2/tengine/mod.ts";
