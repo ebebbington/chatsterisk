54a50a6abbf564794d3cf7a0662e151a22cbb887
ebebbington <ebebbington@users.noreply.github.com>
2021-04-04 00:10:45 +0000

    Update dependencies

--- src/video_socket/tests/deps.ts
+++ src/video_socket/tests/deps.ts
@@ -1,2 +1,2 @@
-export { deferred } from "https://deno.land/std@0.85.0/async/deferred.ts";
-export { Rhum } from "https://deno.land/x/rhum@v1.1.7/mod.ts";
+export { deferred } from "https://deno.land/std@0.92.0/async/deferred.ts";
+export { Rhum } from "https://deno.land/x/rhum@v1.1.9/mod.ts";
