9654c0220a0937f1e57a8e502e11d6296ce3988a
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-06-29 13:47:34 +0100

    fix tests

--- src/server/resources/home_resource.ts
+++ src/server/resources/home_resource.ts
@@ -8,7 +8,7 @@ export default class HomeResource extends Drash.Http.Resource {
     this.response.body = decoder.decode(
       Deno.readFileSync("./public/views/index.dml"),
     );
-    this.response.body = this.response.body.replace(/<% title %>/g, "Home");
+    this.response.body = this.response.body.replace(/<% title %>/g, "Chatsterisk - Home");
     this.response.setCookie({
       name: "X-CSRF-TOKEN",
       value: csrf.token,
