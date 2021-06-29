57363237b92dacfb9be4e529d58206b50c16da94
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-06-29 15:41:23 +0100

    upgrade deps

--- src/server/deps.ts
+++ src/server/deps.ts
@@ -1,4 +1,4 @@
 export { Drash } from "https://deno.land/x/drash@v1.5.0/mod.ts";
-export { Paladin } from "https://deno.land/x/drash_middleware@v0.7.7/paladin/mod.ts";
+export { Paladin } from "https://deno.land/x/drash_middleware@v0.7.8/paladin/mod.ts";
 export { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
-export { CSRF } from "https://deno.land/x/drash_middleware@v0.7.7/csrf/mod.ts";
+export { CSRF } from "https://deno.land/x/drash_middleware@v0.7.8/csrf/mod.ts";
