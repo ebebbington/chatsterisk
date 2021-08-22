bc6aced6a814609dc62072414f538c4e8a662e28
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2021-08-22 09:02:04 +0100

    Update dependencies (#54)
    Co-authored-by: ebebbington <ebebbington@users.noreply.github.com>

--- src/call_socket/tests/deps.ts
+++ src/call_socket/tests/deps.ts
@@ -1,2 +1,2 @@
-export { deferred } from "https://deno.land/std@0.104.0/async/deferred.ts";
+export { deferred } from "https://deno.land/std@0.105.0/async/deferred.ts";
 export { Rhum } from "https://deno.land/x/rhum@v1.1.11/mod.ts";
