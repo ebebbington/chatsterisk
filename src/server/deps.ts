e1a44b90e847d7a0daab8585ff3cc4499247d256
ebebbington <ebebbington@users.noreply.github.com>
2021-10-27 00:08:18 +0000

    choore: Update dependencies

--- src/server/deps.ts
+++ src/server/deps.ts
@@ -1,4 +1,4 @@
-export { Drash } from "https://deno.land/x/drash@v1.5.1/mod.ts";
+export { Drash } from "https://deno.land/x/drash@v2.0.0/mod.ts";
 export { Paladin } from "https://deno.land/x/drash_middleware@v0.7.9/paladin/mod.ts";
 export { config } from "https://deno.land/x/dotenv@v3.0.0/mod.ts";
 export { CSRF } from "https://deno.land/x/drash_middleware@v0.7.9/csrf/mod.ts";
