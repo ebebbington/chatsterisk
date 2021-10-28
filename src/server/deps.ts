dffc1ef29199ebc75290c2cba255ac14dd6cf063
ebebbington <ebebbington@users.noreply.github.com>
2021-10-28 00:08:20 +0000

    choore: Update dependencies

--- src/server/deps.ts
+++ src/server/deps.ts
@@ -1,6 +1,6 @@
 export * as Drash from "https://deno.land/x/drash@v2.0.0/mod.ts";
 export { PaladinService as Paladin } from "https://deno.land/x/drash@v2.0.0/src/services/paladin/paladin.ts";
-export { config } from "https://deno.land/x/dotenv@v3.0.0/mod.ts";
+export { config } from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
 export { CSRFService as CSRF } from "https://deno.land/x/drash@v2.0.0/src/services/csrf/csrf.ts";
 import { TengineService as Tengine } from "https://deno.land/x/drash@v2.0.0/src/services/tengine/tengine.ts";
 export const tengine = new Tengine({
